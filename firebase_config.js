515d206747ceca956958bc9a0c6b3c2b77a157e9
ali <umair.ashraf@brainxtech.com>
2018-12-05 15:28:12 +0500

    first commit

---
+++ firebase_config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "apiKey-68",
+    authDomain: "projectId.firebaseapp.com",
+    databaseURL: "https://databaseName.firebaseio.com",
+    projectId: "projectId",
+    storageBucket: "bucket.appspot.com",
+    messagingSenderId: "MessagingSenderId"
+};
+firebase.initializeApp(config);
